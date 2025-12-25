60236196b7088f99d1fa99f425416f2d414ccb37
islam0122 <duishobaevislam01@gmail.com>
2025-12-25 14:20:22 +0600

    Initial project setup (React + TypeScript + Tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind работает!
+      </h1>
+    </div>
+  );
+}
+
